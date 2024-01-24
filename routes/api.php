a002721ac88313b0ab70f5384f692e343b35ed82
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-01-24 17:20:55 +0100

    updated api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\LeadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('project', [ProjectController::class, 'index']);
 Route::get('project/{id}', [ProjectController::class, 'show']);
+
+Route::post('contacts', [LeadController::class,'store']);
