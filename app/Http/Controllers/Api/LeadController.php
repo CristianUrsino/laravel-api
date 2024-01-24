5c3698e79ac320ec6b6b52cd579144b25f2359dc
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-01-24 17:17:01 +0100

    added lead controller

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NewContact;
+
+class LeadController extends Controller
+{
+   
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all()); // Linea di debug che può essere rimossa
+        $data = $request->all();
+        // $validator = Validator::make($data, [
+        //     'name' => 'required',
+        //     'email' => 'required|email',
+        //     'address' => 'required',
+        //     'message' => 'required',
+        // ]);
+        // if ($validator->fails()) {
+        //     return response()->json([
+        //         'success' => false,
+        //         'errors' => $validator->errors()
+        //     ]);
+        // }
+        $newLead = new Lead();
+        $newLead->fill($data);
+        $newLead->save();
+
+        Mail::to('xxxxxx@gmail.com')->send(new NewContact($newLead)); 
+
+        return response()->json([
+            'success' => true,
+            // 'data' => $newLead
+        ]);
+    }
+
+    
+}
