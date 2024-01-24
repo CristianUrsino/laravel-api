4475bec57b88f65173968a859e5a7ccc91abce69
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-01-24 17:20:09 +0100

    added NewContact and view

---
+++ resources/views/mails/new-contact-mail.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <h1>ciao admin</h1>
+    <p>
+        hai ricevuto un messaggio, ecco i dettagli: <br>
+        Nome: {{$lead->name}} <br>
+        Email: {{$lead->email}} <br>
+        address: {{$lead->address}} <br>
+        Address: {{$lead->message}} 
+    </p>
+</div>
